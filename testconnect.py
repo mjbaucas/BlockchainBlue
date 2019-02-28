bfed50c053d64293ee92360af3c77fcfd0e0d6bc
mjbaucas <mjbaucas@gmail.com>
2019-02-27 21:33:35 -0500

    added documentation

--- testconnect.py
+++ testconnect.py
@@ -1,5 +1,7 @@
-import bluetooth
+from bluetooth.ble import DiscoveryService
 
-nearby_devices = bluetooth.discover_devices()
+service = DiscoveryService()
+devices = service.discover(2)
 
-print(nearby_devices)
+for address, name in devices.items():
+    print("name: {}, address: {}".format(name, address))
