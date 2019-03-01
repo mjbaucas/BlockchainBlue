6953bd7e1e24254c2b0bc89bc13af72f216a68c1
mjbaucas <mjbaucas@gmail.com>
2019-03-01 18:02:30 -0500

    Created inf loop for server and client

--- bleclient.py
+++ bleclient.py
@@ -1,12 +1,13 @@
 from bluetooth import *
+import time
 
-# Create the client socket
-client_socket=BluetoothSocket( RFCOMM )
 
-client_socket.connect(("00:12:D2:5A:BD:E4", 3))
 
-client_socket.send("Hello World")
-
-print "Finished"
-
-client_socket.close()
+while(True):
+    # Create the client socket
+    client_socket=BluetoothSocket( RFCOMM )
+    client_socket.connect(("B8:27:EB:F1:6C:F3", 3))
+    client_socket.send("Hello World")
+    print "Finished"
+    client_socket.close()
+    time.sleep(3)
