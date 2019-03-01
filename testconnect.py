c7480d5d61c7376a8b89aef7532c911fa8e6c905
mjbaucas <mjbaucas@gmail.com>
2019-02-28 22:14:06 -0500

    Minor fixes to try statements

--- testconnect.py
+++ testconnect.py
@@ -1,4 +1,5 @@
 from gattlib import GATTRequester
+import time
 
 req = GATTRequester("B8:27:EB:F1:GC:F3", False)
 #name = req.read_by_uuid("00002a00-0000-1000-8000-00805f9b34fb")[0]
@@ -13,4 +14,4 @@ while(check == 1):
         check = 1
     except Exception as e:
         check = 0
-        
+        time.sleep(0.5)
--- testsend.py
+++ testsend.py
@@ -3,12 +3,12 @@ import time
 
 req = GATTRequester("B8:27:EB:B4:34:F1", False)
 
-check = 0
+check = 1
 while(check == 1):
 	try:
 		req.write_by_handle(0x10, str(bytearray([14, 4, 56])))
-	    check = 0
+		check = 0
 	except Exception as e:
 		check = 1
-		time.sleep(1)
+		time.sleep(0.5)
 
