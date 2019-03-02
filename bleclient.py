f69e40f07a1a343a1131a56236c40bc0ffdfc191
mjbaucas <mjbaucas@gmail.com>
2019-03-01 19:13:44 -0500

    Moved socket initialization outside while loop

--- bleclient.py
+++ bleclient.py
@@ -1,13 +1,13 @@
 from bluetooth import *
 import time
 
-
+# Create the client socket
+client_socket=BluetoothSocket( RFCOMM )
+client_socket.connect(("B8:27:EB:F1:6C:F3", 3))
 
 while(True):
-    # Create the client socket
-    client_socket=BluetoothSocket( RFCOMM )
-    client_socket.connect(("B8:27:EB:F1:6C:F3", 3))
     client_socket.send("Hello World")
     print "Finished"
-    client_socket.close()
     time.sleep(3)
+
+client_socket.close()
