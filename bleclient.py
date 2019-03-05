80965a932cb71da4936a8df2add94641c3ccac81
mjbaucas <mjbaucas@gmail.com>
2019-03-04 20:30:36 -0500

    Moved code within while loop

--- bleclient.py
+++ bleclient.py
@@ -1,10 +1,8 @@
 from bluetooth import *
 import time
 
-# Create the client socket
-client_socket=BluetoothSocket( RFCOMM )
-
 while(True):
+    client_socket=BluetoothSocket( RFCOMM )
     client_socket.connect(("B8:27:EB:F1:6C:F3", 3))
     client_socket.send("Hello World")
     print "Finished"
--- bleserver.py
+++ bleserver.py
@@ -1,16 +1,14 @@
 from bluetooth import *
 import time
 
-server_socket=BluetoothSocket( RFCOMM )
 
-server_socket.bind(("", 3 ))
 
 while(True):
+    server_socket=BluetoothSocket( RFCOMM )
+    server_socket.bind(("", 3 ))
     server_socket.listen(1)
     client_socket, address = server_socket.accept()
     data = client_socket.recv(1024)
-
     print "received [%s]" % data
     client_socket.close()
-
-server_socket.close()
+    server_socket.close()
