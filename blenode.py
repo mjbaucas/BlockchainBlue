990e67f124c9f1f8f7cf74dffaa551dc743ab4af
mjbaucas <mjbaucas@gmail.com>
2019-03-04 21:20:57 -0500

    Retry senging message until message successfully received

--- blenode.py
+++ blenode.py
@@ -4,19 +4,22 @@ import time
 route = "B8:27:EB:F1:6C:F3"
 
 while(True):
-    try:
-        client_socket=BluetoothSocket( RFCOMM )
-        client_socket.connect((route, 3))
-        client_socket.send("Hello World")
-        print "Finished"
-        client_socket.close()
-        time.sleep(2)
-    except Exception as e:
-        print e
+    sent = False
+    while(not sent)
+        try:
+            client_socket=BluetoothSocket( RFCOMM )
+            client_socket.connect((route, 3))
+            client_socket.send("Hello World")
+            print "Finished"
+            client_socket.close()
+            time.sleep(2)
+            sent = True
+        except Exception as e:
+            print e
     
     
     server_socket=BluetoothSocket( RFCOMM )
-    server_socket.bind(("", 2 ))
+    server_socket.bind(("", 3 ))
     server_socket.listen(1)
     client_socket, address = server_socket.accept()
     data = client_socket.recv(1024)
