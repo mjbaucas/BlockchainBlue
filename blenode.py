53bcdedb2c1e8508b0162735d469d81cc8f9cbb8
mjbaucas <mjbaucas@gmail.com>
2019-03-11 12:32:51 -0400

    Added comments and hashing algorithm for ledger

--- blenode.py
+++ blenode.py
@@ -2,6 +2,7 @@ from bluetooth import *
 from node import Node
 import time
 
+# Temporary Ledger
 ledger = [
 	"B8:27:EB:9F:A5:AC",
 	"B8:27:EB:8C:3E:7C",
@@ -12,9 +13,11 @@ ledger = [
 node = Node(ledger)
 
 while(True):
+    # Reset variables
     sent = False
     trial = 0
     
+    # Attempt to send packet to other device
     while(not sent and trial <= 3):
         try:
             route = node.pull_from_ledger()
@@ -29,12 +32,16 @@ while(True):
             time.sleep(1)
             print e
     
-    
+    # Wait for packet from other device
     server_socket=BluetoothSocket( RFCOMM )
     server_socket.bind(("", 3 ))
     server_socket.listen(8)
     client_socket, address = server_socket.accept()
-    data = client_socket.recv(1024)
-    print "received [%s]" % node.read_packet(data)
+    packet = client_socket.recv(1024)
+    data = node.read_packet(packet)
+    print "received [{}]".format(data)
+    print "{}".format(node.verify_ledger(data['Chain']))
+    
+    # Close sockets
     client_socket.close()
     server_socket.close()
--- node.py
+++ node.py
@@ -1,11 +1,17 @@
 import commands
-from aes_cipher import AESCipher
+import ast 
+import hashlib
 from time import time
 
+from aes_cipher import AESCipher
+
+
 class Node:
+	# Initialization
 	def __init__(self, ledger):
 		self.bdaddr = self.get_local_bdaddr()
 		
+		# Set ledger count to position of local address in current ledger
 		try:
 			index = ledger.index(self.bdaddr)
 			ledger.remove(self.bdaddr)
@@ -14,17 +20,19 @@ class Node:
 			if self.ledger_count >= limit:
 				self.ledger_count = self.ledger_count%limit
 		except ValueError:
+			# If local address was not found in ledger, set count to 0 >> TEMPORARY >> Unrecognized devices should not be allowed access to the loop
 			self.ledger_count = 0
 		
 		self.ledger = ledger
 		self.cipher = AESCipher()
 		self.key = "KEY"
-		
-		
+	
+	# Get Bluetooth MAC address from device		
 	def get_local_bdaddr(self):
 		_ , output = commands.getstatusoutput("hciconfig")
 		return output.split("hci0:")[1].split("BD Address: ")[1].split(" ")[0] 
 	
+	# Pull an address from the ledger
 	def pull_from_ledger(self):
 		limit = len(self.ledger)
 		value = self.ledger[self.ledger_count]
@@ -32,10 +40,19 @@ class Node:
 		if self.ledger_count >= limit:
 			self.ledger_count = self.ledger_count%limit
 		return value
-
+    
+    # Get encrypted version of ledger
 	def get_ledger(self):
-		return self.cipher.encrypt(self.ledger, self.key)
+		sha = hashlib.sha256()
+		sha.update(str(self.ledger))
+		return sha.hexdigest()
+		#sha.update((str(self.index) + str(self.timestamp) + str(self.transactions) + str(self.previous_hash)).encode('utf-8'))
+	
+	# Verify ledger
+	def verify_ledger(self, hashed_ledger):
+		return hashed_ledger == hashlib.sha256(str(self.ledger)).hexdigest()
 
+	# Create consensus packet
 	def get_consensus_packet(self):
 		message = {
 			"Source": self.bdaddr,
@@ -44,6 +61,7 @@ class Node:
 		}
 		return self.cipher.encrypt(message, self.key)
 	
+	# Read packet
 	def read_packet(self, packet):
-		return self.cipher.decrypt(packet, self.key)
-		
+		dict_str = self.cipher.decrypt(packet, self.key)
+		return ast.literal_eval(dict_str)
